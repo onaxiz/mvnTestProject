a6d299dc2df61cd4c5b6d3380b7fb64a6e2fec5a
emaksimova <onaxiz@tutanota.com>
2021-03-15 16:03:04 +0300

    logging into file

--- mvndz/src/main/java/com/gd/internship/me/DzMvn.java
+++ mvndz/src/main/java/com/gd/internship/me/DzMvn.java
@@ -9,7 +9,6 @@ public class DzMvn {
     static Logger logger = Logger.getLogger(DzMvn.class);
 
     public static void main(String[] args) {
-        BasicConfigurator.configure();
         Faker faker = new Faker();
         logger.warn(faker.aviation().METAR());
         logger.info(faker.app().version());

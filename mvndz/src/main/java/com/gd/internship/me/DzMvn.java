518b9b1f4d256ce20b0df00da2a06086e560ffe2
emaksimova <onaxiz@tutanota.com>
2021-03-15 11:12:45 +0300

    Log4j and javafaker output into console

--- mvndz/src/main/java/com/gd/internship/me/DzMvn.java
+++ mvndz/src/main/java/com/gd/internship/me/DzMvn.java
@@ -1,15 +1,17 @@
 package com.gd.internship.me;
 
-import com.github.javafaker.Faker;
 
+import com.github.javafaker.Faker;
+import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 
 public class DzMvn {
-//    static Logger logger = Logger.getLogger(DzMvn.class.getName());
+    static Logger logger = Logger.getLogger(DzMvn.class);
 
     public static void main(String[] args) {
-//        Faker faker = new Faker();
-//        logger.warn(faker.aviation().METAR());
-//        logger.info(faker.app().version());
+        BasicConfigurator.configure();
+        Faker faker = new Faker();
+        logger.warn(faker.aviation().METAR());
+        logger.info(faker.app().version());
     }
 }
